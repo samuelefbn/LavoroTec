0c73f3ea766f203dde62b97576b5062a14eb717b
samuelefbn <57558756+samuelefbn@users.noreply.github.com>
2020-06-02 23:19:47 +0200

    Add files via upload

--- fase di sviluppo/jdindondan/JHome.java
+++ fase di sviluppo/jdindondan/JHome.java
@@ -111,7 +111,11 @@ public class JHome extends javax.swing.JFrame {
 
         buttonFedelta.setBackground(new java.awt.Color(153, 255, 204));
         buttonFedelta.setText("CARTA FEDELTA");
-        buttonFedelta.setEnabled(false);
+        buttonFedelta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonFedeltaActionPerformed(evt);
+            }
+        });
 
         buttonCatalogo.setBackground(new java.awt.Color(153, 255, 204));
         buttonCatalogo.setText("-> CATALOGO <-");
@@ -270,6 +274,11 @@ public class JHome extends javax.swing.JFrame {
         JPrenotazione nuova = new JPrenotazione(this,true);
         nuova.setVisible(true);
     }//GEN-LAST:event_buttonPrenotatiActionPerformed
+
+    private void buttonFedeltaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonFedeltaActionPerformed
+        JFrameFedelta nuovo= new JFrameFedelta();
+        nuovo.setVisible(true);
+    }//GEN-LAST:event_buttonFedeltaActionPerformed
 
     /**
      * @param args the command line arguments
